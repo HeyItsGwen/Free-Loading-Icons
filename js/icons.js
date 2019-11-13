facb24789266ad97bddb74215a1750c82c7e7428
Gwen <itsgwenhey@gmail.com>
2019-11-12 22:16:27 -0600

    Add working 'get code' button
    Use Jquery to change css values on click, currently opens all instances of the class, and I can't for the life of me figure out how to make it just the instances in the clicked element. So many 'this' attempts.

--- js/icons.js
+++ js/icons.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const clickShow = () => {
+let clickCount = 0;
 
-};
+$('.opener').click( () => {
+    if(clickCount == 0){
+        $('.hideShow').css('visibility', 'visible');
+        $('.hideShow').css('position', 'static');
+        clickCount += 1;
+        console.log(clickCount);
+    }
+});
+
+$('.closer').click( () => {
+    if(clickCount == 1){
+        $('.hideShow').css('visibility', 'hidden');
+        $('.hideShow').css('position', 'absolute');
+        clickCount -= 1;
+    }
+});
+
