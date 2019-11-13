06478450109d111ac9e074107b4c6d022373516d
Gwen <itsgwenhey@gmail.com>
2019-11-12 22:56:02 -0600

    make get code functionality better
    Improved the 'get code' buttons functionality to open and close individually

--- js/icons.js
+++ js/icons.js
@@ -1,21 +1,14 @@
 'use strict';
 
-let clickCount = 0;
-
 $('.opener').click( () => {
-    if(clickCount == 0){
-        $('.hideShow').css('visibility', 'visible');
-        $('.hideShow').css('position', 'static');
-        clickCount += 1;
-        console.log(clickCount);
-    }
+    const hideShow = $(event.currentTarget).parent().find('.hideShow');
+    hideShow.css('visibility', 'visible');
+    hideShow.css('position', 'static');
 });
 
 $('.closer').click( () => {
-    if(clickCount == 1){
-        $('.hideShow').css('visibility', 'hidden');
-        $('.hideShow').css('position', 'absolute');
-        clickCount -= 1;
-    }
+    const hideShow = $(event.currentTarget).parent().parent().find('.hideShow');
+    hideShow.css('visibility', 'hidden');
+    hideShow.css('position', 'absolute');
 });
 
